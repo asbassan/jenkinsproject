b9e4dd80b6d3950e633635216d1f093e49bf6d4d
student <asbassan@gmail.com>
2021-04-27 16:53:26 +0000

    adding the initial files

---
+++ src/test/java/com/myproject/mywebsite/MywebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.mywebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MywebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
